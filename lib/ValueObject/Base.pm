b442d0cd74d10103c01cbc773ac052b7f4e559f7
Alasdair Keyes <alasdair@akeyes.co.uk>
2017-01-30 23:30:01 +0000

    Add POD for Base package

--- lib/ValueObject/Base.pm
+++ lib/ValueObject/Base.pm
@@ -1,5 +1,6 @@
 package ValueObject::Base;
 
+
 use strict;
 use warnings;
 use Carp;
@@ -31,3 +32,50 @@ sub value {
 }
 
 1;
+
+=head1 NAME
+
+ValueObject::Base - Base object for all value objects
+
+=head1 SYNOPSIS
+
+  use base qw/ ValueObject::Base /;
+
+  sub validate {
+    my $self    = shift;
+    my $value   = shift || '';
+    
+    # if $value validates
+    # 	return 1
+    # else
+    # 	Throw exception on failer
+  }
+
+=head1 DESCRIPTION
+
+ValueObject::Base is a base class from which all ValueObjects extend.
+Only the validate() sub routine needs defining when writing a custom ValueObject
+
+=head1 METHODS
+
+=over 4
+
+=item new($value = '')
+
+Creates a new object
+
+=item validate()
+
+Called during instantiation to validate $value
+
+=item value()
+
+Returns the raw value as passed in at instantiation
+
+=back
+
+=head1 SEE ALSO
+
+L<Carp>
+
+=cut
