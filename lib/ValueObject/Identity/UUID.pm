43f326c84992081ed61c75c2dfda216585a48771
Alasdair Keyes <alasdair@akeyes.co.uk>
2017-02-04 14:51:02 +0000

    Refactor UUID to act as a factory

--- lib/ValueObject/Identity/UUID.pm
+++ lib/ValueObject/Identity/UUID.pm
@@ -3,15 +3,34 @@ package ValueObject::Identity::UUID;
 use strict;
 use warnings;
 use Carp;
+use ValueObject::Identity::UUIDv1;
+use ValueObject::Identity::UUIDv2;
+use ValueObject::Identity::UUIDv3;
+use ValueObject::Identity::UUIDv4;
+use ValueObject::Identity::UUIDv5;
 use base qw/ ValueObject::Base /;
 use ValueObject::Identity::UUID::Exception::Invalid;
 
 sub validate {
     my $self    = shift;
     my $value   = shift || '';
-    croak (ValueObject::Identity::UUID::Exception::Invalid->new($value))
-        unless ($value =~ /^[0-9a-f]{8}\-[0-9a-f]{4}\-[1-5][0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12}$/);
-    return 1;
+
+    foreach my $uuid_type (
+		'ValueObject::Identity::UUIDv1',
+		'ValueObject::Identity::UUIDv2',
+		'ValueObject::Identity::UUIDv3',
+		'ValueObject::Identity::UUIDv4',
+		'ValueObject::Identity::UUIDv5'
+	) {
+        my $uuid_object = eval {
+            $uuid_type->new($value);
+        };
+        if ($uuid_object) {
+            return $uuid_object;
+        }
+    }
+
+    croak (ValueObject::Identity::UUID::Exception::Invalid->new($value));
 }
 
 1;
