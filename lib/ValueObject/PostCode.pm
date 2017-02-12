59fd84bd46d41a943ef21a2620cd04f9aa9aa1d0
Alasdair Keyes <alasdair@akeyes.co.uk>
2017-02-12 15:03:19 +0000

    Add in perldoc for Post Code value object

--- lib/ValueObject/PostCode.pm
+++ lib/ValueObject/PostCode.pm
@@ -6,7 +6,7 @@ use Carp;
 use base qw/ ValueObject::Base /;
 use ValueObject::PostCode::Exception::Invalid;
 
-# Guide to valid regex format taken from https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/283357/ILRSpecification2013_14Appendix_C_Dec2012_v1.pdf
+
 
 sub validate {
     my $self    = shift;
@@ -17,3 +17,46 @@ sub validate {
 }
 
 1;
+
+=head1 NAME
+
+ValueObject::PostCode - Value object to represent uk post codes
+
+=head1 SYNOPSIS
+
+  use ValueObject::PostCode;
+
+  my $vo = ValueObject::PostCode->new('B1 1BG');
+
+=head1 DESCRIPTION
+
+ValueObject::PostCode provides Value Object for validation of uk
+post codes - case insensitive.
+
+# Guide to valid regex format taken from https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/283357/ILRSpecification2013_14Appendix_C_Dec2012_v1.pdf
+
+=head1 METHODS
+
+=over 4
+
+=item new($value = '')
+
+Creates a new object
+
+Throws ValueObject::PostCode::Exception::Invalid if $value is invalid
+
+=item validate()
+
+Called during instantiation to validate $value
+
+=item value()
+
+Returns the raw value as passed in at instantiation
+
+=back
+
+=head1 SEE ALSO
+
+L<ValueObject::Base>
+
+=cut
